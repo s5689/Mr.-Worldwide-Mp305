850b25e39ad5269ff5ad5c5175235294af5c221f
s5689 <cguillermo@kingdeportes.com>
2023-02-10 08:00:45 -0400

    initial

---
+++ js/controls.js
@@ -0,0 +1,75 @@
+const mrbill = 'https://soundcloud.com/mrbillstunes/thwekz';
+const hudson = 'https://soundcloud.com/hudsonlee/sets/flockcall';
+
+const swish = '5thOUCqiHgpstZ0beZvmou';
+const midi = '0mZa5qa3VjZGsbm183O1zF';
+
+let controller;
+
+setTimeout(() => {
+  const callback = (e) => {
+    console.log(e);
+    e.iframeElement.id = 'spotify';
+    e.iframeElement.className = 'players';
+    controller = e;
+  };
+
+  SpotifyIframeApi.createController(document.getElementById('spotify-container'), {}, callback);
+}, 2000);
+
+function but(e) {
+  if (e === 0) {
+    $('#soundcloud').attr(
+      'src',
+      `https://w.soundcloud.com/player/?url=${mrbill}&auto_play=true&show_comments=false&show_user=true&hide_related=true`
+    );
+    $('#soundcloud').css('display', 'block');
+  }
+  if (e === 1) {
+    $('#soundcloud').attr(
+      'src',
+      `https://w.soundcloud.com/player/?url=${hudson}&auto_play=true&show_comments=false&show_user=true&hide_related=true`
+    );
+    $('#soundcloud').css('display', 'block');
+  }
+  if (e === 2) {
+    $('#spotify').attr('src', `https://open.spotify.com/embed/track/${swish}?&theme=0`);
+    $('#spotify').css('display', 'block');
+  }
+  if (e === 3) {
+    /*
+    $('#spotify').attr('src', `https://open.spotify.com/embed/track/${midi}?&theme=0`);
+    */
+    $('#spotify').css('display', 'block');
+
+    controller.loadUri(`spotify:track:${swish}`);
+    controller.play();
+    console.log(controller);
+  }
+}
+
+function stop(e = 2) {
+  if (e === 0) {
+    $('#soundcloud').attr('src', '');
+    $('#soundcloud').css('display', 'none');
+  }
+
+  if (e === 1) {
+    $('#spotify').attr('src', '');
+    $('#spotify').css('display', 'none');
+  }
+
+  if (e === 2) {
+    $('#soundcloud').attr('src', '');
+    $('#soundcloud').css('display', 'none');
+    $('#spotify').attr('src', '');
+    $('#spotify').css('display', 'none');
+  }
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+window.but = but;
+window.stop = stop;
---
+++ js/db.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { addDoc, getDocs, collection } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAyacVFGoIKMMtkH7znpi1ESh2G0UlSKQI',
+  authDomain: 'sound-micron-377214.firebaseapp.com',
+  projectId: 'sound-micron-377214',
+  storageBucket: 'sound-micron-377214.appspot.com',
+  messagingSenderId: '71286093345',
+  appId: '1:71286093345:web:127ea171b8d5531e945560',
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const SONGS_TABLE = 'songs';
+
+export const saveSong = (data) => addDoc(collection(db, SONGS_TABLE), data);
+export const getSongs = () => getDocs(collection(db, SONGS_TABLE));
