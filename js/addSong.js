07ff298d173f2e0efaee34cfe77e5d7e8dba4a4d
s5689 <cguillermo@kingdeportes.com>
2023-02-11 08:23:38 -0400

    + Verificacion de canciones en SoundCloud
    WIP: Verificacion de canciones en Spotify

--- js/addSong.js
+++ js/addSong.js
@@ -1,5 +1,7 @@
 import { saveSong } from './db';
 
+const SOUNDCLOUD = 'SOUNDCLOUD';
+const SPOTIFY = 'SPOTIFY';
 let isOpen = false;
 let inputs = [];
 
@@ -23,14 +25,93 @@ export function closeAddSong() {
 
 export function saveAddSong() {
   getInputs();
-  const data = {
+  const link = inputs[3].value;
+
+  switch (validate(link)) {
+    case SOUNDCLOUD:
+      testSC(link);
+      break;
+
+    case SPOTIFY:
+      testSP(link);
+      break;
+
+    default:
+      handleError();
+      break;
+  }
+}
+
+// Comprobar formato del link
+function validate(link) {
+  const foundSC = link.search('soundcloud');
+  const foundSP = link.search('spotify');
+
+  if (foundSC !== -1) return SOUNDCLOUD;
+  if (foundSP !== -1) return SPOTIFY;
+
+  return;
+}
+
+// Guardar Datos en Firebase
+function sendToDB(e) {
+  const inputData = {
     name: inputs[0].value,
     artist: inputs[1].value,
     album: inputs[2].value,
     link: inputs[3].value,
+    time: getTime(e),
+    fullTime: e.full_duration,
   };
 
-  console.log(data);
+  console.log(inputData);
+
+  $('#sc-tester').attr('src', '');
+}
+
+// Comprobar link de SoundCloud
+function testSC(link) {
+  let success = false;
+
+  // Intentar reproducir link
+  $('#sc-tester').attr('src', `https://w.soundcloud.com/player/?url=${link}`);
+  const iframe = SC.Widget(document.getElementById('sc-tester'));
+
+  // Si el link es correcto, enviar los datos
+  iframe.bind(SC.Widget.Events.READY, () => {
+    iframe.getCurrentSound((e) => {
+      if (!success) {
+        success = true;
+        sendToDB(e);
+      }
+
+      iframe.unbind(SC.Widget.Events.READY);
+    });
+  });
+
+  // Si demora mas de 5 minutos, enviar un error
+  setTimeout(() => {
+    if (!success) handleError();
+  }, 5000);
+}
+
+// Comprobar link de Spotify
+function testSP(link) {
+  console.log('sp');
+}
+
+function handleError() {
+  console.log('a');
+}
+
+function handleSuccess() {}
+
+function getTime(e) {
+  const time = parseInt(e.full_duration / 1000, 10);
+  const m = Math.floor(time / 60);
+  const s = time - m * 60;
+
+  return `${m}:${s < 10 ? `0${s}` : s}`;
 }
 
 function getInputs() {
