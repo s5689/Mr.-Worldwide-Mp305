45d3b32245bb90ec336cb568d7c157d0db014be2
s5689 <lemirl0ug@gmail.com>
2023-04-27 21:30:34 -0400

    Desktop bugfix
    El reproductor no aplicaba correctamente el css sobre el reproductor de youtube.
    
    Error relacionado a un cambio interno de la api del mismo.

--- desktop/js/youtube.js
+++ desktop/js/youtube.js
@@ -9,7 +9,7 @@ export function loadYT() {
     },
     events: {
       onReady: () => {
-        controller.h.className = 'players';
+        controller.g.className = 'players';
       },
     },
   });
