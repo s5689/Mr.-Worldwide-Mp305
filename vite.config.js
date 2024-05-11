dc5a2ed2141468179fffca084cd9df98b6c68265
Wedixtor <lemirl0ug@gmail.com>
2024-05-11 17:16:54 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,6 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         desktop: resolve(__dirname, 'desktop/index.html'),
         mobile: resolve(__dirname, 'mobile/index.html'),
-        /*
-        main: resolve(__dirname, 'MWW-Web/index.html'),
-        desktop: resolve(__dirname, 'MWW-Web/desktop/index.html'),
-        mobile: resolve(__dirname, 'MWW-Web/mobile/index.html'),
-        */
       },
     },
   },
